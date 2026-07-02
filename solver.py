6c15231a6df8b249cc15d068cdb1785c24586740
wltt1004 <rrvagas3@gmail.com>
2026-07-02 23:56:59 +0000

    build codex/succ-pig1 (succession:0.85.0-succ) on main 9f107e6

--- solver.py
+++ solver.py
@@ -23,8 +23,8 @@ from minotaur_subnet.shared.types import ExecutionPlan, Interaction
 
 logger = logging.getLogger(__name__)
 
-SOLVER_NAME = os.environ.get("MINOTAUR_SOLVER_NAME", "apex-split-router-b")
-SOLVER_VERSION = os.environ.get("MINOTAUR_SOLVER_VERSION", "2.4.0")
+SOLVER_NAME = os.environ.get("MINOTAUR_SOLVER_NAME", "putty-king-solver")
+SOLVER_VERSION = os.environ.get("MINOTAUR_SOLVER_VERSION", "0.85.0-succ")
 SOLVER_AUTHOR = os.environ.get("MINOTAUR_SOLVER_AUTHOR", "martindev0207")
 
 _BASE = 8453
