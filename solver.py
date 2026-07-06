0fcbd09c2e09cbd3cae6e7cb4108558f4a4bd233
star <star@gmail.com>
2026-07-06 19:19:07 +0000

    beam1: fresh KyberSwap capture for USDC->0x00000e7e blind spot

--- solver.py
+++ solver.py
@@ -22,8 +22,8 @@ from minotaur_subnet.shared.types import ExecutionPlan, Interaction
 
 logger = logging.getLogger(__name__)
 
-SOLVER_NAME = os.environ.get("MINOTAUR_SOLVER_NAME", "viking-mino-solver")
-SOLVER_VERSION = os.environ.get("MINOTAUR_SOLVER_VERSION", "124.0.0")
+SOLVER_NAME = os.environ.get("MINOTAUR_SOLVER_NAME", "beam1-fresh-router")
+SOLVER_VERSION = os.environ.get("MINOTAUR_SOLVER_VERSION", "1.0.0")
 SOLVER_AUTHOR = os.environ.get("MINOTAUR_SOLVER_AUTHOR", "martindev0207")
 
 _KING_REPLAY_CACHE = None
