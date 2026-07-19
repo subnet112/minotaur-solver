3dc6b406680074aeb9341e3a744db6dba5b77d80
Stalker <stalkervrm@proton.me>
2026-07-19 11:08:48 +0200

    champion: private submission via PR #3 (round round-e29740806-n1)
    source: fiready126/sn112-solver-private@ab9aa6d79a79c2a0e9b7a55ca391757c5d31f858

--- solver.py
+++ solver.py
@@ -31,9 +31,9 @@ from minotaur_subnet.sdk.intent_solver import SolverMetadata
 from minotaur_subnet.shared.types import ExecutionPlan, Interaction
 def _solver_c():
     logger = logging.getLogger(__name__)
-    _PUTTY_FINAL_BRAND = 'hydra-vector-router'
+    _PUTTY_FINAL_BRAND = 'hydra-thread-router'
     SOLVER_NAME = os.environ.get('MINOTAUR_SOLVER_NAME', _PUTTY_FINAL_BRAND)
-    SOLVER_VERSION = os.environ.get('MINOTAUR_SOLVER_VERSION', '2.6.1f')
+    SOLVER_VERSION = os.environ.get('MINOTAUR_SOLVER_VERSION', '2.6.1c')
     SOLVER_AUTHOR = os.environ.get('MINOTAUR_SOLVER_AUTHOR', 'hydra')
     globals().update(locals())
 _solver_c()
